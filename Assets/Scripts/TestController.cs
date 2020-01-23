2adb14cbfcd827a2a59c1c318d8952858a69e0e2
KadyVMorse <kadmorse@uat.edu>
2020-01-23 12:28:33 -0700

    Added GameManager,InputController,TestController

--- Assets/Scripts/TestController.cs
+++ Assets/Scripts/TestController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(TankData))]
+[RequireComponent(typeof(TankMotor))]
 public class TestController : MonoBehaviour
 {
     private TankMotor motor;
