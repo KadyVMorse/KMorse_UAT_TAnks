ed6bf789ae3d15e26c691e24edf090d668446da7
KadyVMorse <kadmorse@uat.edu>
2020-01-24 01:00:28 -0700

    Worked on enemy and cannon scripts

--- Assets/Scripts/CannonBall.cs
+++ Assets/Scripts/CannonBall.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class CannonBall : MonoBehaviour
 {
     public float damage;
- 
+    public float timer;
 
-
-     void Update()
+    void Start()
     {
-        // after set ammount of time,destroy the cannon ball.
-
+        
     }
 
-  void OnCollisionEnter(Collision collision)
+    void Update()
+    {
+        //after a certain amount of time bullet disappers
+        timer += 1.0F * Time.deltaTime;
+
+        if (timer >= 1)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    
+
+    void OnCollisionEnter(Collision collision)
     {
         //Apply damage to what we collided with 
-
+     
         //Destroy the cannon ball if it collides with anything
         Destroy(gameObject);
     }
---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public GameObject cannonBall;
+    public GameObject firePoint;
+
+    private TankData data;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        data = gameObject.GetComponent<TankData>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Shoot()
+    {
+        //instaniate a bullet
+
+        GameObject ourCannonBall = Instantiate(cannonBall, firePoint.transform.position, firePoint.transform.rotation);
+        CannonBall cannonBallComponet = ourCannonBall.GetComponent<CannonBall>();
+        Rigidbody cannonBody = ourCannonBall.GetComponent<Rigidbody>();
+        //apply force
+
+        cannonBody.AddForce(data.shellForce * transform.forward, ForceMode.Impulse);
+
+        //tell how much damage the cannon ball should do
+        cannonBallComponet.damage = data.damageDone;
+    }
+}
+
