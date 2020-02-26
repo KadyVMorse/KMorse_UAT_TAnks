397bc0d6456c90fbe0e66b182cd965d6572750b2
KadyVMorse <kadmorse@uat.edu>
2020-02-26 01:08:03 -0700

    PowerUps and Map Generatation
    Started working on powerups and Map Generation

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Powerup : MonoBehaviour
+{
+    public float speedModifier;
+    public float heatlthModifier;
+    public float maxHealthModifier;
+    public float fireRateModifier;
+
+    public float duration;
+    public bool isPermanent;
+
+    public void OnActivate(TankData target)
+    {
+        target.moveSpeed += speedModifier;
+        target.health += heatlthModifier;
+        target.maxHealth += maxHealthModifier;
+        target.fireRate += fireRateModifier;
+    }
+    public void OnDeactivate(TankData target)
+    {
+        target.moveSpeed -= speedModifier;
+        target.health -= heatlthModifier;
+        target.maxHealth -= maxHealthModifier;
+        target.fireRate -= fireRateModifier;
+    }
+
+
+}
